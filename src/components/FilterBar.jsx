2edb8365f17ad0ad83bea741a678088b297bb210
Joaquin Borja <joaquingregory.borja@gmail.com>
2026-07-28 16:34:45 +0800

    Implemented Filter Bar

---
+++ src/components/FilterBar.jsx
@@ -0,0 +1,24 @@
+import {STATUS_LABELS, STATUS_OPTIONS} from "../utils/constants";
+
+export default function FilterBar({ statusFilter, setStatusFilter, sortOrder, setSortOrder}) {
+    return (
+        <div style={{
+            display: "flex",
+            gap: 12,
+            marginBottom: 16
+        }}>
+            <select
+             value={statusFilter}
+             onchange={(e) => setStatusFilter(e.target.value)}>
+                <option value="all">
+                    All statuses
+                </option>
+                {STATUS_OPTIONS.map((s) => (
+                    <option key={s} value={s}>
+                        {STATUS_LABELS[s]}
+                    </option>
+                ))}
+            </select>
+        </div>
+    )
+} 
